33a867bbacb91aa1a23b2fd73881bdba9e54022b
OmarGeno <106829277+OmarGeno@users.noreply.github.com>
2023-04-23 22:37:08 +0300

    Add files via upload

---
+++ lib/styles/my_icons.dart
@@ -0,0 +1,4 @@
+const cameraIcon = './assets/camera.svg';
+const dots = './assets/dots3.svg';
+const search = './assets/search.svg';
+const group = './assets/group.svg';
---
+++ lib/users.dart
@@ -0,0 +1,21 @@
+import 'users.class.dart';
+var users = [
+  UserRow(
+    name: 'Omar',
+  ),
+  UserRow(
+    name: 'Andrei',
+  ),
+  UserRow(
+    name: 'Eduard',
+  ),
+  UserRow(
+    name: 'Madalin',
+  ),
+  UserRow(
+    name: 'Rami',
+  ),
+  UserRow(
+    name: 'Marin',
+  ),
+];
